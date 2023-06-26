ab3e7d67fc5066387271b245877d79efaa137600
ArnoldRamodike <riccoarnold23@gmail.com>
2023-06-26 14:13:23 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+    return(
+        <div>
+           <section className="flex-start flex-col padding mb-16">
+                <h1>Category</h1>
+                <h1>posts</h1>
+                <h1>Loadmore</h1>
+           </section>
+        </div>
+    )
+}
+
+export default Home;
